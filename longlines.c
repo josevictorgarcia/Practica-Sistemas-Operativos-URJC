e3608935b93cf1e234366e272a8bfb6efbc1d6f4
jose victor garcia llorente <victorgarciallorente20@gmail.com>
2023-10-26 12:11:38 +0200

    longlines function implemented

--- longlines.c
+++ longlines.c
@@ -3,30 +3,19 @@
 #include<string.h>
 #include<stdbool.h>
 
-int ordenar(char **cadena, int n){
-    int indice;
-    indice=0;
-    char *nuevo;
-    nuevo=cadena[n];
-    if(cadena[indice]==NULL){                                //Caso mas basico: Si cadena esta vacio, basta con hacer que cadena[0]=nuevo
-        cadena[indice]=(char*)malloc(1024*sizeof(char));
-        cadena[indice]=nuevo;
-    }
-    else{
-        while(strlen(cadena[indice])>strlen(nuevo) && indice<n){
-            indice++;
-        }
-        if (indice<n){
+int ordenar(char **cadena, int tope, int max){          //max=4, tope=i
+    //printf("Cadena actual: %s", cadena[max]);
+    //printf("Cadena con menos caracteres: %s", cadena[tope]);
+    //printf("Cadena con más caracteres: %s", cadena[0]);
+    
+    int j;
+    for(j=0;j<max;j++){
+        if(strlen(cadena[j])<strlen(cadena[max])){
             char *aux;
             aux = (char*)malloc(1024*sizeof(char));
-            strcpy(aux, nuevo);
-            int j;
-            j=n-1;
-            while(j!=indice){
-                cadena[j]=cadena[j-1];
-                j--;
-            }
-            cadena[indice]=aux;
+            strcpy(aux, cadena[max]);
+            strcpy(cadena[max], cadena[j]);
+            strcpy(cadena[j], aux);
         }
     }
 
@@ -40,10 +29,20 @@ int main(){
     char **cadena;
 
     cadena = (char**)malloc((n+1)*sizeof(char*));
-    cadena[n] = (char*)malloc(1024*sizeof(char));
+    //cadena[n] = (char*)malloc(1024*sizeof(char));
 
+    for(i=0; i<(n+1); i++){
+        cadena[i] = (char*)malloc(1024*sizeof(char));
+    }
+
+    i=0;
+    fgets(cadena[i], 1024, stdin);                  //Caso mas basico: Como la cadena esta vacia, basta con hacer que cadena[0]=nuevalinea
     while(fgets(cadena[n], 1024, stdin)!=NULL){
-        ordenar(cadena, n);
+        ordenar(cadena, i, n);
+        if(i!=(n-1)){
+            i++;
+        }
+        cadena[n]=(char*)malloc(1024*sizeof(char));
     }
 
     for (i=0; i<n; i++){
