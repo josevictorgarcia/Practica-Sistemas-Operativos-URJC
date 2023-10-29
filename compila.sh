fe79fdd7ed17416c29946035500283654b809b27
jose victor garcia llorente <victorgarciallorente20@gmail.com>
2023-10-29 18:38:06 +0100

    more comments

--- compila.sh
+++ compila.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -c libreria.c -o libreria.o;
-ar -rv libreria.a libreria.o;
-gcc -o test test.c -L./ -lreria;
+gcc -c libreria.c -o libreria.o;    #Genera el archivo objeto compilando el archivo fuente
+ar -rv libreria.a libreria.o;       #Genera la libreria e inserta el archivo objeto generado
+gcc -o test test.c -L./ -lreria;    #Genera el archivo ejecutable enlazando la libreria generada
--- test.c
+++ test.c
@@ -5,20 +5,20 @@
 
 int main(int argc, char *argv[]){
 
-    switch(argc){
-        case 2:
+    switch(argc){                                                                       //Comprobacion de argumentos
+        case 2:                                                                         //Si se introduce solo un argumento (nombre de la funcion), el valor por defecto son 10 lineas
             if(strcmp(argv[1], "-head")==0){head(10);}
             else if (strcmp(argv[1], "-tail")==0){tail(10);}
             else if (strcmp(argv[1], "-longlines")==0){longlines(10);}
-            else{printf("No se reconoce %s como argumento\n", argv[1]);}
+            else{printf("No se reconoce %s como argumento\n", argv[1]);}                //En caso de que el primer (y unico) argumento no sea valido
         break;
-        case 3:
+        case 3:                                                                         //Si se introducen dos argumentos, el segundo indica el numero de lineas que se devolveran
                 if(strcmp(argv[1], "-head")==0){head(atoi(argv[2]));}
                 else if (strcmp(argv[1], "-tail")==0){tail(atoi(argv[2]));}
                 else if (strcmp(argv[1], "-longlines")==0){longlines(atoi(argv[2]));}
-                else{printf("No se reconoce %s como argumento\n", argv[1]);}
+                else{printf("No se reconoce %s como argumento\n", argv[1]);}            //En caso de que el primer argumento no sea valido. El segundo argumento no da error ya que si se introduce cualquier cosa que no sea un numero, atoi() devolvera 0 y por tanto se devolveran 0 lineas
         break;
-        default: 
+        default:                                                                        //En caso de que se introduzcan 0 o mas argumentos
             printf("El numero de argumentos (%d) debe ser 1 o 2\n", argc-1);
         break;
     }
